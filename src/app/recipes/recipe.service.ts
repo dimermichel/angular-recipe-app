6b9b3ad4e4530a701064eb9e0f9675cea419b384
Michel Maia <dimermichel@gmail.com>
2020-09-08 16:42:09 -0400

    Add fetch data to service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,30 +7,37 @@ import { Subject } from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Lasagna',
-      'The best Lasagna recipe',
-      'https://images.media-allrecipes.com/userphotos/720x405/3359675.jpg',
-      [
-        new Ingredient('Pasta', 3),
-        new Ingredient('Meat', 1),
-        new Ingredient('Tomatoes', 4),
-      ]),
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Lasagna',
+  //     'The best Lasagna recipe',
+  //     'https://images.media-allrecipes.com/userphotos/720x405/3359675.jpg',
+  //     [
+  //       new Ingredient('Pasta', 3),
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Tomatoes', 4),
+  //     ]),
 
-    new Recipe(
-      'Cookies',
-      'The Best Chocolate Chip Cookie recipe',
-      'https://joyfoodsunshine.com/wp-content/uploads/2016/01/best-chocolate-chip-cookies-recipe-ever-no-chilling-1-e1549147195343.jpg',
-      [
-        new Ingredient('Chocolate', 5),
-        new Ingredient('Milk', 1),
-        new Ingredient('Eggs', 3),
-        new Ingredient('Wheat Flour', 1),
-      ]),
-  ];
+  //   new Recipe(
+  //     'Cookies',
+  //     'The Best Chocolate Chip Cookie recipe',
+  //     'https://joyfoodsunshine.com/wp-content/uploads/2016/01/best-chocolate-chip-cookies-recipe-ever-no-chilling-1-e1549147195343.jpg',
+  //     [
+  //       new Ingredient('Chocolate', 5),
+  //       new Ingredient('Milk', 1),
+  //       new Ingredient('Eggs', 3),
+  //       new Ingredient('Wheat Flour', 1),
+  //     ]),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService){}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 import { RecipeService } from "../recipes/recipe.service";
 import { environment } from '../../environments/environment';
+import { Recipe } from "../recipes/recipe.model";
 
 
 @Injectable({providedIn: 'root'})
@@ -17,6 +19,18 @@ export class DataStorageService {
     this.http.put(`${this.baseUrl}/recipes.json`, recipes)
     .subscribe((response) => {
       console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>(`${this.baseUrl}/recipes.json`)
+    .pipe(map(recipes => {
+      return recipes.map(recipe => {
+        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+      })
+    }))
+    .subscribe((recipes) => {
+      this.recipeService.setRecipes(recipes);
     });
   }
 }
