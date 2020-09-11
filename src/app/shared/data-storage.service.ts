7cab0e87439d791691293b17e3038a5120b630cb
Michel Maia <dimermichel@gmail.com>
2020-09-10 21:34:09 -0400

    Add Auth logic to persist data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map, tap } from "rxjs/operators";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
 import { RecipeService } from "../recipes/recipe.service";
 import { environment } from '../../environments/environment';
 import { Recipe } from "../recipes/recipe.model";
+import { AuthService } from "../auth/auth.service";
 
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
   baseUrl: string;
   constructor(private http: HttpClient,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
                 this.baseUrl = environment.BASE_URL;
               }
 
@@ -24,12 +26,14 @@ export class DataStorageService {
 
   fetchRecipes() {
     return this.http.get<Recipe[]>(`${this.baseUrl}/recipes.json`)
-    .pipe(map(recipes => {
-      return recipes.map(recipe => {
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+    .pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        })
+      }), tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
       })
-    }), tap((recipes) => {
-      this.recipeService.setRecipes(recipes);
-    }));
+    );
   }
 }
